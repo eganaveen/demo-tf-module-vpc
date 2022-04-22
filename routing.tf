9eb0d39f9f29947c4c95808608a4e35c612ca7ff
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-22 15:59:11 +0530

    peering

--- routing.tf
+++ routing.tf
@@ -4,14 +4,14 @@ resource "aws_route_table_association" "rt-association" {
   subnet_id = element(aws_subnet.tfvpc-subnet1.*.id, count.index )
 }
 
-resource "aws_route" "route-to-default-vpc-rt" {
+resource "aws_route" "route-to-dev-vpc-rt" {
   route_table_id = var.DEFAULT_VPC_ROUTE_TABLE_ID
   destination_cidr_block = var.vpccidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
 }
 
-#resource "aws_route" "route-to-dev-vpc-rt" {
-#  route_table_id = var.DEV_ROUTE_TABLE_ID
-#  destination_cidr_block = var.DEFAULT_VPC_CIDR
-#  vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
-#}
+resource "aws_route" "route-to-default-vpc-rt" {
+  route_table_id = var.DEV_ROUTE_TABLE_ID
+  destination_cidr_block = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
+}
