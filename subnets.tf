d819b81b82fbea5c8a32c2730bf18b75a03c0457
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-22 12:52:39 +0530

    subnet

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "tfvpc-subnet1" {
   count = length(var.subnet1cidr)
-  cidr_block = element(var.subnet1cidr,count.index)
-  availability_zone = element(var.AZ,count.index)
+  cidr_block = element(var.subnet1cidr, count.index)
+  availability_zone = element(var.AZ, count.index)
   vpc_id     = aws_vpc.tfvpc.id
   tags = {
     Name = "${var.ENV}-${element(var.AZ,count.index)}"
