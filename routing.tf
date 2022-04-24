6ba7862b1e700a34556edf92df5f9fe5c42e000d
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-24 12:05:37 +0530

    fetch dev-route-table-id

--- routing.tf
+++ routing.tf
@@ -1,12 +1,12 @@
 
 resource "aws_default_route_table" "dev-default-route-table-id" {
-  default_route_table_id = aws_vpc.tfvpc.id
+  default_route_table_id = aws_vpc.tfvpc.main_route_table_id
 }
 
 
 
 resource "aws_route_table_association" "rt-association" {
-  route_table_id = aws_default_route_table.dev-default-route-table-id.id
+  route_table_id = aws_default_route_table.dev-default-route-table-id.default_route_table_id
   count = length(aws_subnet.tfvpc-subnet1.*.id)
   subnet_id = element(aws_subnet.tfvpc-subnet1.*.id, count.index )
 }
@@ -18,7 +18,7 @@ resource "aws_route" "route-to-dev-vpc-rt" {
 }
 
 resource "aws_route" "route-to-default-vpc-rt" {
-  route_table_id = aws_default_route_table.dev-default-route-table-id.id
+  route_table_id = aws_default_route_table.dev-default-route-table-id.default_route_table_id
   destination_cidr_block = var.DEFAULT_VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
 }
