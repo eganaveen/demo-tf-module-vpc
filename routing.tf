e229f44abef5b73395d2752bda2ff70d14215ed3
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-24 20:40:06 +0530

    sdvdsv

--- routing.tf
+++ routing.tf
@@ -2,7 +2,9 @@ data "aws_route_table" "default-route-table" {
   vpc_id = var.DEFAULT_VPC_ID
 }
 
-data "aws_internet_gateway" "igw" {}
+data "aws_internet_gateway" "igw" {
+  tags = {Name: igw}
+}
 
   #resource "aws_default_route_table" "system-default-route-table-id" {
   #  default_route_table_id = data.aws_route_table.default-route-table.route_table_id
