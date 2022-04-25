df64aee64b37bf6d120746c322450d4fe9a2b1ee
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-25 11:27:52 +0530

    sddssd

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,19 @@
 resource "aws_subnet" "public-subnet1" {
 #  count = length(var.SUBNET_CIDR)
   cidr_block = element(var.SUBNET_CIDR[0]) //["10.0.0.0/25", "10.0.0.128/25"]
-  availability_zone = element(var.AZ, count.index)   //["us-east-1a","us-east-1b"]
+  availability_zone = element(var.AZ)   //["us-east-1a","us-east-1b"]
   vpc_id     = aws_vpc.tfvpc.id
   tags = {
-    Name = "${var.ENV}-${element(var.AZ,count.index)}"
+    Name = "${var.ENV}-${element(var.AZ)}"
   }
 }
 
 resource "aws_subnet" "private-subnet1" {
   count = length(var.SUBNET_CIDR)
   cidr_block = element(var.SUBNET_CIDR[1]) //["10.0.0.0/25", "10.0.0.128/25"]
-  availability_zone = element(var.AZ, count.index)   //["us-east-1a","us-east-1b"]
+  availability_zone = element(var.AZ)   //["us-east-1a","us-east-1b"]
   vpc_id     = aws_vpc.tfvpc.id
   tags = {
-    Name = "${var.ENV}-${element(var.AZ,count.index)}"
+    Name = "${var.ENV}-${element(var.AZ)}"
   }
 }
