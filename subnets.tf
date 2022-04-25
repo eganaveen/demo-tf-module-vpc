b3f13c44fab5d9cbfcaa57bd3e5e64a928bfc631
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-25 11:37:23 +0530

    sddssd

--- subnets.tf
+++ subnets.tf
@@ -8,12 +8,12 @@ resource "aws_subnet" "public-subnet" {
   }
 }
 
-#resource "aws_subnet" "private-subnet" {
-##  count = length(var.SUBNET_CIDR)
-#  cidr_block = var.SUBNET_CIDR[1] //["10.0.0.0/25", "10.0.0.128/25"]
-#  availability_zone = var.AZ[1]   //["us-east-1a","us-east-1b"]
-#  vpc_id     = aws_vpc.tfvpc.id
-#  tags = {
-#    Name = "${var.ENV}-${var.AZ[1]}"
-#  }
-#}
+resource "aws_subnet" "private-subnet" {
+#  count = length(var.SUBNET_CIDR)
+  cidr_block = var.SUBNET_CIDR[1] //["10.0.0.0/25", "10.0.0.128/25"]
+  availability_zone = var.AZ[1]   //["us-east-1a","us-east-1b"]
+  vpc_id     = aws_vpc.tfvpc.id
+  tags = {
+    Name = "${var.ENV}-${var.AZ[1]}"
+  }
+}
