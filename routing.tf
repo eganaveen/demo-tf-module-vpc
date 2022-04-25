58e30081d64df9787c9ee9aceb31f3d3913130b6
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-25 12:08:58 +0530

    sddssd

--- routing.tf
+++ routing.tf
@@ -20,6 +20,17 @@ resource "aws_route_table_association" "private-rt-association" {
   subnet_id = aws_subnet.private-subnet.id
 }
 
+resource "aws_route" "route-to-default-vpc-rt" {
+  route_table_id = data.aws_route_table.default-route-table.route_table_id
+  destination_cidr_block = var.vpccidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
+}
+
+resource "aws_route" "route-to-dev-vpc-rt" {
+  route_table_id = aws_default_route_table.dev-public-route-table-id.id
+  destination_cidr_block = var.DEFAULT_VPC_ID
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-connection.id
+}
 
 
 #resource "aws_route" "route-to-default-vpc-rt" {
